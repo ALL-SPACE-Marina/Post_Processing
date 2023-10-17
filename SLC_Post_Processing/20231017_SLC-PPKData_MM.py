6a88bb30ff0955a87a565d41d19de7dbc49c756e
mmarinova <mmarinova@all.spae>
2023-10-17 17:46:07 +0100

    Initial Commit

--- SLC_Post_Processing/20231017_SLC-PPKData_MM.py
+++ SLC_Post_Processing/20231017_SLC-PPKData_MM.py
@@ -6,11 +6,17 @@ import pip
 plt.close('all')
 
 # load
-fileName = r'C:\Terminal_Testing\G-Type_Test\SLC3_Tx_F-G_Comparison\ppk_data'
-Th_deg=30.0
+fileName = r'C:\Terminal_Testing\SLC_plotting\Tx_G_vs_F-Type_all_Fq\ppk_data'
+savePath=r'C:\Terminal_Testing\SLC_plotting\Tx_G_vs_F-Type_all_Fq'
+Th_deg_list=[0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0]
 Ph_deg=0.0
-cal_freq=29.5
-meas_freq=29.5
+cal_freq_list=[27.5, 28.0, 28.5, 29.0, 29.5, 30.0, 30.5, 31.0]
+meas_freq_list=[27.5, 28.0, 28.5, 29.0, 29.5, 30.0, 30.5, 31.0]
+
+labelLog = ['F-Type_QR00057', 'G-Type_QR00012']
+fpathLog = [r'/mnt/nfs/data/groups/measurements/TLM_Meas/DVT_G-Type/Tx_TLM/Comparisons/SLC3/Multi_Fq/Raw_Data/F-Type_QR00057',
+            r'/mnt/nfs/data/groups/measurements/TLM_Meas/DVT_G-Type/Tx_TLM/Comparisons/SLC3/Multi_Fq/Raw_Data/G-Type_QR00012']
+
 
 dirScript = os.getcwd()
 os.chdir(dirScript)
@@ -31,7 +37,7 @@ def thetaphi_to_azel(theta, phi):
      
     return az
 
-def plot__gainVstheta(fpath):
+def plot__gainVstheta(fpath, cal_freq, meas_freq):
     global dfPlot, df, x, y, columns, acu
 
     # load
@@ -48,39 +54,12 @@ def plot__gainVstheta(fpath):
     df = df[(df["entry_type"] == 'gain_at_requested_angle')]
     df = df[(df["fpath_parent"] == fpath)]
     acu=np.array(df['acu_version'])[0]
+    print(acu)
 
     x = np.array(df['theta_deg'])
     y = np.array(df['Gain_dB'])
 
-
-
-labelLog = ['F-Type_QR00057', 'G-Type_QR00012', 'G-Type_QR00017']
-
-plt.figure(figsize=([7,6]))
-fpathLog = [r'/mnt/nfs/data/groups/measurements/TLM_Meas/DVT_G-Type/Tx_TLM/Comparisons/SLC3/Raw_Data/F-Type_QR00057',
-            r'/mnt/nfs/data/groups/measurements/TLM_Meas/DVT_G-Type/Tx_TLM/Comparisons/SLC3/Raw_Data/G-Type_QR00012',
-            r'/mnt/nfs/data/groups/measurements/TLM_Meas/DVT_G-Type/Tx_TLM/Comparisons/SLC3/Raw_Data/G-Type_QR00017']
-for i in range(len(fpathLog)):
-    fpath = fpathLog[i]
-    plot__gainVstheta(fpath)
-    plt.plot(x, y, 's', markeredgecolor='k', markersize=10, label = labelLog[i])
-    plt.xlabel('Theta [deg]'); plt.ylabel('Beam 1 gain [dB]\n(at req. angle)')
-    plt.yticks(np.linspace(0,100,num=51))
-    plt.xticks(np.linspace(-100,100,num=21))
-    plt.xlim([-10,80]); plt.ylim([30,70]);# plt.ylim([65, 75])
-    plt.grid('on')
-    plt.legend(loc='lower left')
-    plt.title('SW: ' + acu + '\nFreq = ' + str(meas_freq) + ' GHz' + '\nb1: Th=' + 'X' + ', Phi=' + str(Ph_deg), fontsize=15)
-    plt.tight_layout()
-    plt.savefig(r'C:\Terminal_Testing\G-Type_Test\SLC3_Tx_F-G_Comparison'+'\\' + 'Pointing_angle_Phi_' + str(Ph_deg) + 'Freq_' + str(meas_freq)+'GHz.png', dpi=400)
-
-    
-    
-    
-
-
-
-def plot__gainVsfreq(fpath):
+def plot__gainVsfreq(fpath,Th_deg):
     global dfPlot, df, x, y, columns, phi_meas_freq
 
     # load
@@ -103,27 +82,50 @@ def plot__gainVsfreq(fpath):
     x = np.array(df['freq_GHz'])
     y = np.array(df['Gain_dB'])
 
-labelLog = ['F-Type_QR00057', 'G-Type_QR00012', 'G-Type_QR00017']
 
-plt.figure(figsize=([7,6]))
-fpathLog = [r'/mnt/nfs/data/groups/measurements/TLM_Meas/DVT_G-Type/Tx_TLM/Comparisons/SLC3/Raw_Data/F-Type_QR00057',
-            r'/mnt/nfs/data/groups/measurements/TLM_Meas/DVT_G-Type/Tx_TLM/Comparisons/SLC3/Raw_Data/G-Type_QR00012',
-            r'/mnt/nfs/data/groups/measurements/TLM_Meas/DVT_G-Type/Tx_TLM/Comparisons/SLC3/Raw_Data/G-Type_QR00017']
 
-for i in range(len(fpathLog)):
-    fpath = fpathLog[i]
-    plot__gainVsfreq(fpath)
-    plt.plot(x, y, 's', markeredgecolor='k', markersize=10, label = labelLog[i])
-    plt.xlabel('freq [GHz]'); plt.ylabel('Beam 1 gain [dB]\n(at req. angle)')
-    plt.yticks(np.linspace(0,100,num=21))
-    #plt.xticks(np.linspace(-100,100,num=21))
-    plt.xlim([27.5,31.0]); plt.ylim([0,70]);# plt.ylim([65, 75])
-    plt.grid('on')
-    plt.legend(loc='lower left')
-    plt.title('SW: ' + acu + '\nFreq = ' + ' X' + '\nb1: Th=' + str(Th_deg) + ', Phi=' + str(Ph_deg), fontsize=15)
-    plt.tight_layout()
-    plt.savefig(r'C:\Terminal_Testing\G-Type_Test\SLC3_Tx_F-G_Comparison' + '\\' +  'Frequency_comparison_Th_'+ str(Th_deg) + '_Phi_' + str(Ph_deg)+'.png', dpi=400)
+
+for k in range(len(cal_freq_list)):
+    plt.figure(figsize=([7,6]))
+
+    for i in range(len(fpathLog)):
+        fpath = fpathLog[i]
+        plot__gainVstheta(fpath, cal_freq_list[k], meas_freq_list[k])
+        plt.plot(x, y, 's', markeredgecolor='k', markersize=10, label = labelLog[i])
+        plt.xlabel('Theta [deg]'); plt.ylabel('Beam 1 gain [dB]\n(at req. angle)')
+        plt.yticks(np.linspace(0,100,num=51))
+        plt.xticks(np.linspace(-100,100,num=21))
+        plt.xlim([-10,80]); plt.ylim([30,70]);# plt.ylim([65, 75])
+        plt.grid('on')
+        plt.legend(loc='lower left')
+        plt.title('SW: ' + acu + '\nFreq = ' + str(meas_freq_list[k]) + ' GHz' + '\nb1: Th=' + 'X' + ', Phi=' + str(Ph_deg), fontsize=15)
+        plt.tight_layout()
+        plt.savefig(savePath+'\\' + 'Pointing_angle_Phi_' + str(Ph_deg) + 'Freq_' + str(meas_freq_list[k])+'GHz.png', dpi=400)
+    plt.close('all')
     
     
-    sb_mute = 'OFF'
+    
+
+
+
+
+
+for p in range(len(Th_deg_list)):
+
+    for i in range(len(fpathLog)):
+        fpath = fpathLog[i]
+        plot__gainVsfreq(fpath, Th_deg_list[p])
+        plt.plot(x, y, 's', markeredgecolor='k', markersize=10, label = labelLog[i])
+        plt.xlabel('freq [GHz]'); plt.ylabel('Beam 1 gain [dB]\n(at req. angle)')
+        plt.yticks(np.linspace(0,100,num=21))
+        #plt.xticks(np.linspace(-100,100,num=21))
+        plt.xlim([27.0,31.5]); plt.ylim([0,70]);# plt.ylim([65, 75])
+        plt.grid('on')
+        plt.legend(loc='lower left')
+        plt.title('SW: ' + acu + '\nFreq = ' + ' X' + '\nb1: Th=' + str(Th_deg_list[p]) + ', Phi=' + str(Ph_deg), fontsize=15)
+        plt.tight_layout()
+        plt.savefig(r'C:\Terminal_Testing\SLC_plotting\Tx_G_vs_F-Type_all_Fq' + '\\' +  'Frequency_comparison_Th_'+ str(Th_deg_list[p]) + '_Phi_' + str(Ph_deg)+'.png', dpi=400)
+    plt.close('all')
+    
+#sb_mute = 'OFF'
     
